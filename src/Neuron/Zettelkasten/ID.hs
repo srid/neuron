c3df2f7dee8effbf97081564fd9ce3086c83ca08
Sridhar Ratnakumar <srid@srid.ca>
2020-03-07 20:25:34 -0500

    cleanup

--- src/Neuron/Zettelkasten/ID.hs
+++ src/Neuron/Zettelkasten/ID.hs
@@ -1,10 +1,6 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
@@ -14,26 +10,17 @@ module Neuron.Zettelkasten.ID
     Connection (..),
     ZettelConnection,
     zettelIDDate,
-    connectionScheme,
     parseZettelID,
     mkZettelID,
-    zettelIDsFromMMark,
   )
 where
 
-import Control.Foldl (Fold (..))
-import qualified Data.Set as Set
 import qualified Data.Text as T
 import Data.Time.Calendar
 import Data.Time.Format
 import Lucid
 import Path
 import Relude
-import Text.MMark (MMark, runScanner)
-import Text.MMark.Extension (Inline (..))
-import qualified Text.MMark.Extension as Ext
-import qualified Text.URI as URI
-import Text.URI.QQ (scheme)
 
 -- Short Zettel ID encoding datetime.
 --
@@ -67,23 +54,6 @@ zettelIDDate =
       Nothing ->
         error "Bad day"
 
-type ZettelConnection = (Connection, ZettelID)
-
--- | Represent the connection between zettels
-data Connection
-  = -- | A folgezettel points to a zettel that is conceptually a part of the
-    -- parent zettel.
-    Folgezettel
-  | -- | Any other ordinary connection (eg: "See also")
-    OrdinaryConnection
-  deriving (Eq, Show, Enum, Bounded)
-
--- TODO: avoid DRY with Link.hs
-connectionScheme :: Connection -> URI.RText l
-connectionScheme = \case
-  Folgezettel -> [scheme|z|]
-  OrdinaryConnection -> [scheme|zcf|]
-
 instance ToHtml ZettelID where
   toHtmlRaw = toHtml
   toHtml = toHtml . unZettelID
@@ -98,42 +68,13 @@ mkZettelID fp = either (error . toText . displayException) id $ do
   (name, _) <- splitExtension $ filename fp
   pure $ ZettelID $ toText $ toFilePath name
 
--- | TODO: This should support multiple protocols
-mkZettelIDFromMarkdownLink :: MarkdownLink -> Maybe (Connection, ZettelID)
-mkZettelIDFromMarkdownLink MarkdownLink {..} =
-  listToMaybe $ flip mapMaybe connections $ \c ->
-    if URI.uriScheme markdownLinkUri == Just (connectionScheme c)
-      then Just (c, ZettelID markdownLinkText)
-      else Nothing
-  where
-    connections :: [Connection]
-    connections = [minBound .. maxBound]
+type ZettelConnection = (Connection, ZettelID)
 
-zettelIDsFromMMark :: MMark -> [(Connection, ZettelID)]
-zettelIDsFromMMark = mapMaybe mkZettelIDFromMarkdownLink . extractLinks
-
--- Utility
-
-data MarkdownLink
-  = MarkdownLink
-      { markdownLinkText :: Text,
-        markdownLinkUri :: URI.URI
-      }
-  deriving (Eq, Ord)
-
--- | Extract all links from the Markdown document
-extractLinks :: MMark -> [MarkdownLink]
-extractLinks = Set.toList . Set.fromList . flip runScanner (Fold go [] id)
-  where
-    go acc blk = acc <> concat (fmap f (relevantInlines blk))
-    f = \case
-      Link inner uri _title ->
-        [MarkdownLink (Ext.asPlainText inner) uri]
-      _ ->
-        []
-    relevantInlines = \case
-      Ext.Naked xs -> toList xs
-      Ext.Paragraph xs -> toList xs
-      Ext.OrderedList _ xs -> concat $ concat $ fmap (fmap relevantInlines) xs
-      Ext.UnorderedList xs -> concat $ concat $ fmap (fmap relevantInlines) xs
-      _ -> []
+-- | Represent the connection between zettels
+data Connection
+  = -- | A folgezettel points to a zettel that is conceptually a part of the
+    -- parent zettel.
+    Folgezettel
+  | -- | Any other ordinary connection (eg: "See also")
+    OrdinaryConnection
+  deriving (Eq, Show, Enum, Bounded)
