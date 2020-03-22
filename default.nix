6aaf1ea20686627f723c21d7e9c252ecb009ff39
Sridhar Ratnakumar <srid@srid.ca>
2020-03-22 11:50:51 -0400

    Advance rib
    Fixes #19

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
-  ribRevision = "43950d766ea3ee8faf6e4248ab2a56e98924b7d1";
+  ribRevision = "43760b5";
 
   inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
   excludeContent = path: typ: 
