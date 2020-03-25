7d52faba9d7b989d55eeb571c3086e0feded2477
Sridhar Ratnakumar <srid@srid.ca>
2020-03-25 08:39:33 -0400

    Move neuron-search to its own directory

--- default.nix
+++ default.nix
@@ -22,19 +22,8 @@ in {
 }:
 
 let 
-  neuronSearchScript = pkgs.writeShellScriptBin "neuron-search"
-    ''
-      set -euo pipefail
-      NOTESDIR=''${1}
-      cd ''${NOTESDIR}
-      ${pkgs.ripgrep}/bin/rg --no-heading --no-line-number --sort path "title:" *.md \
-        | ${pkgs.fzf}/bin/fzf --tac --no-sort -d ':' -n 3.. \
-          --preview '${pkgs.bat}/bin/bat --style=plain --color=always {1}' \
-          --bind 'ctrl-j:execute(xdg-open https://localhost:8080/(echo {1} | ${pkgs.gnused}/bin/sed "s/\.md/.html/g"))+abort' \
-        | ${pkgs.gawk}/bin/awk -F: "{printf \"''${NOTESDIR}%s\", \$1}"
-    '';
-  additional-packages = pkgs: with pkgs; 
-  [ neuronSearchScript
+  additional-packages = pkgs:
+  [ (pkgs.callPackage ./src-script/neuron-search { inherit pkgs; })
   ];
   neuron = import rib { 
     inherit root name additional-packages; 
