e154306ae04a8b80f848a7eef1dad74be868a3c8
Sridhar Ratnakumar <srid@srid.ca>
2020-04-04 15:36:47 -0400

    Fix nix based git rev detection work

--- default.nix
+++ default.nix
@@ -9,7 +9,25 @@ let
   excludeContent = path: typ: 
     let d = baseNameOf (toString path);
     in !(d == "guide" && typ == "directory");
-  neuronRoot = pkgs.lib.cleanSourceWith { filter = excludeContent; src = gitignoreSource ./.; };
+  projectRoot = ./.;
+  neuronSrc = pkgs.lib.cleanSourceWith { filter = excludeContent; src = gitignoreSource projectRoot; };
+  gitDescribe = pkgs.runCommand "neuron-gitDescribe" 
+    { buildInputs = [ pkgs.git ]; }
+    ''
+      mkdir $out
+      git -C ${projectRoot} describe --long --always > $out/output
+    '';
+  neuronRoot = pkgs.runCommand "neuron" { buildInputs = [ pkgs.git gitDescribe ]; }
+    ''
+    mkdir $out
+    cp -r -p ${neuronSrc}/* $out/
+    chmod -R u+w $out/
+    GITDESC=`cat ${gitDescribe}/output`
+    cat << EOF > $out/src/Neuron/Zettelkasten/Version.hs
+    module Neuron.Zettelkasten.Version where
+    version = "$GITDESC"
+    EOF
+    '';
 in {
 # Rib library source to use
   rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
@@ -29,17 +47,10 @@ let
 in import rib { 
     inherit root name additional-packages; 
     source-overrides = {
-      neuron = neuronRoot;
       # Until https://github.com/obsidiansystems/which/pull/6 is merged
       which = builtins.fetchTarball "https://github.com/srid/which/archive/5061a97.tar.gz";
       # Generate git version because gitignore prevent using gitRev
-      neuronVersion = pkgs.runCommand "version" { buildInputs = [ pkgs.git ]; }
-      ''
-      cat << EOF > ${neuronRoot}/src/Neuron/Zettelkasten/Version.hs
-      module Neuron.Zettelkasten.Version where
-      version = $(git describe --long --always)
-      EOF
-      '';
+      neuron = neuronRoot;
     } // source-overrides;
     overrides = self: super: with pkgs.haskell.lib; {
       # We must add neuron-search as a runtime dependency to the 'neuron'
