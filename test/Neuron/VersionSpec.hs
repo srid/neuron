d7ea194ae61d834ad7bf6d12f00b7abf9154ebf9
Sridhar Ratnakumar <srid@srid.ca>
2020-04-06 12:49:18 -0400

    More tests

--- test/Neuron/VersionSpec.hs
+++ test/Neuron/VersionSpec.hs
@@ -21,7 +21,14 @@ spec = do
     it "should contain the git rev" $ do
       pending
   describe "Version comparison" $ do
-    it "must compare correctly" $ do
+    it "must compare simple versions" $ do
       "0.2" `shouldSatisfy` olderThan
       "0.1" `shouldNotSatisfy` olderThan -- This is current version
       "0.0" `shouldNotSatisfy` olderThan
+    it "must compare full versions" $ do
+      "0.2.1.2" `shouldSatisfy` olderThan
+      "0.2.3" `shouldSatisfy` olderThan
+      "0.1.0.0" `shouldNotSatisfy` olderThan -- This is current version
+      "0.0.1.0" `shouldNotSatisfy` olderThan
+    it "must compare within same major version" $ do
+      "0.1.0.2" `shouldSatisfy` olderThan -- 0.1.0.0 is the current version
