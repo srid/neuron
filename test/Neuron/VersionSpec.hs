e65bc85e2aece85ec6f3b8434d7553e6883b5379
Sridhar Ratnakumar <srid@srid.ca>
2020-04-08 11:11:19 -0400

    Update minVersion

--- test/Neuron/VersionSpec.hs
+++ test/Neuron/VersionSpec.hs
@@ -15,11 +15,11 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "Application version" $ do
-    -- TODO: More checks. This one is trivial and unnecessary, just for testing tests.
     it "should have dots" $ do
       neuronVersion `shouldSatisfy` T.isInfixOf "."
     it "should contain the git rev" $ do
       pending
+  -- TODO: Check minVersion in Default.dhall is same as the one in Paths_neuron
   describe "Version comparison" $ do
     it "must compare simple versions" $ do
       "0.3" `shouldSatisfy` olderThan
