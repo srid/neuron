bd37acfd42bb0da840adaa23e0eda7170bf0dd3c
Sridhar Ratnakumar <srid@srid.ca>
2020-04-11 11:44:08 -0400

    refactor

--- test/Neuron/Zettelkasten/IDSpec.hs
+++ test/Neuron/Zettelkasten/IDSpec.hs
@@ -16,9 +16,9 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "Zettel ID" $ do
+    let day = fromGregorian 2020 3 19
     context "date id parsing" $ do
-      let day = fromGregorian 2020 3 19
-          zid = Z.ZettelDateID day 1
+      let zid = Z.ZettelDateID day 1
       it "parses a zettel ID" $ do
         Z.parseZettelID "2011401" `shouldBe` zid
       it "parses a zettel ID from zettel filename" $ do
@@ -33,9 +33,8 @@ spec = do
       it "parses a custom zettel ID from zettel filename" $ do
         Z.mkZettelID "20abcde.md" `shouldBe` zid
         Z.zettelIDSourceFileName zid `shouldBe` "20abcde.md"
-    do
-      let dateID = Z.ZettelDateID (fromGregorian 2020 4 11) 1
-          customID = Z.ZettelCustomID "20abcde"
-      it "converts ID to JSON string" $ do
-        Aeson.toJSON customID `shouldBe` Aeson.String "20abcde"
-        Aeson.toJSON dateID `shouldBe` Aeson.String "2014601"
+    context "JSON encoding" $ do
+      let zid = Z.ZettelDateID day 1
+      it "Converts ID to text when encoding to JSON" $ do
+        Aeson.toJSON (Z.ZettelCustomID "20abcde") `shouldBe` Aeson.String "20abcde"
+        Aeson.toJSON zid `shouldBe` Aeson.String "2011401"
