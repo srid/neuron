8d0216e6a45db4848b805f1659f5f7ed2173c029
Sridhar Ratnakumar <srid@srid.ca>
2020-04-15 14:19:37 -0400

    Fix build

--- src-bin/Main.hs
+++ src-bin/Main.hs
@@ -15,16 +15,17 @@ import qualified Clay as C
 import Development.Shake
 import Lucid
 import Main.Utf8
-import qualified Neuron.Zettelkasten as Z
 import qualified Neuron.Config as Z
+import qualified Neuron.Web.Generate as Z
 import qualified Neuron.Web.Route as Z
 import qualified Neuron.Web.View as Z
+import Neuron.CLI (run)
 import Relude
 import qualified Rib
 import Rib.Extra.CSS (googleFonts, stylesheet)
 
 main :: IO ()
-main = withUtf8 $ Z.run generateSite
+main = withUtf8 $ run generateSite
 
 generateSite :: Action ()
 generateSite = do
@@ -45,7 +46,7 @@ renderPage config r val@(s, _, _) = html_ [lang_ "en"] $ do
         stylesheet "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
         stylesheet "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
         style_ [type_ "text/css"] $ C.renderWith C.compact [] $ style config
-        googleFonts $ [headerFont, bodyFont, monoFont]
+        googleFonts [headerFont, bodyFont, monoFont]
         when (Z.mathJaxSupport config) $
           with (script_ mempty) [id_ "MathJax-script", src_ "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js", async_ ""]
   body_ $ do
