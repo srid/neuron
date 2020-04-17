2864266767a183674896505a553d9da0a77a454d
felko <baxbaxwalanuksiwe@gmail.com>
2020-04-17 18:33:45 +0200

    Add tests for hierarchical tags in query

--- test/Neuron/Zettelkasten/QuerySpec.hs
+++ test/Neuron/Zettelkasten/QuerySpec.hs
@@ -20,6 +20,10 @@ spec =
       parseQueryString "zquery://search" `shouldBe` Right []
     it "Parse single tag" $
       parseQueryString "zquery://search?tag=foo" `shouldBe` Right [ByTag $ TagPattern "foo"]
+    it "Parse hierarchical tag" $ do
+      parseQueryString "zquery://search?tag=foo/bar" `shouldBe` Right [ByTag $ TagPattern "foo/bar"]
+    it "Parse tag pattern" $ do
+      parseQueryString "zquery://search?tag=foo/**/bar/*/baz" `shouldBe` Right [ByTag $ TagPattern "foo/**/bar/*/baz"]
     it "Parse multiple tags" $
       parseQueryString "zquery://search?tag=foo&tag=bar"
         `shouldBe` Right [ByTag $ TagPattern "foo", ByTag $ TagPattern "bar"]
