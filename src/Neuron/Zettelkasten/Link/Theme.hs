8cc6800ff817f1e265ca8c834b18556494fdd567
Sridhar Ratnakumar <srid@srid.ca>
2020-04-18 15:14:32 -0400

    Add forgotten file

---
+++ src/Neuron/Zettelkasten/Link/Theme.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Neuron.Zettelkasten.Link.Theme where
+
+import Relude
+import qualified Text.URI as URI
+
+data LinkTheme
+  = LinkTheme_Default
+  | LinkTheme_Simple
+  | LinkTheme_WithDate
+  deriving (Eq, Show, Ord)
+
+linkThemeFromUri :: URI.URI -> Maybe LinkTheme
+linkThemeFromUri uri =
+  listToMaybe $ flip mapMaybe (URI.uriQuery uri) $ \case
+    URI.QueryFlag _ -> Nothing
+    URI.QueryParam (URI.unRText -> key) (URI.unRText -> val) ->
+      case key of
+        "linkTheme" ->
+          case val of
+            "default" -> Just LinkTheme_Default
+            "simple" -> Just LinkTheme_Simple
+            "withDate" -> Just LinkTheme_WithDate
+            _ -> error $ "Unknown link theme: " <> val
+        _ -> Nothing
