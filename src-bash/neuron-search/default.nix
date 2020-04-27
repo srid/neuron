061e2a478989031337de1d416c87f42f44183471
Jared Weakly <jaredweakly@gmail.com>
2020-04-27 11:11:36 -0700

    Remove ctrl-j binding

--- src-bash/neuron-search/default.nix
+++ src-bash/neuron-search/default.nix
@@ -14,7 +14,6 @@ pkgs.writeShellScriptBin "neuron-search"
     ${pkgs.ripgrep}/bin/rg --no-heading --no-line-number --with-filename --sort path "''${FILTERBY}" *.md \
       | ${pkgs.fzf}/bin/fzf --tac --no-sort -d ':' -n ''${SEARCHFROMFIELD}.. \
         --preview '${pkgs.bat}/bin/bat --style=plain --color=always {1}' \
-        --bind 'ctrl-j:execute(xdg-open http://localhost:8080/$(echo {1} | ${pkgs.gnused}/bin/sed "s/\.md/.html/g"))+abort' \
       | ${pkgs.gawk}/bin/awk -F: "{printf \"''${NOTESDIR}/%s\", \$1}" \
       | ${pkgs.findutils}/bin/xargs -r ''${OPENCMD}
   ''
