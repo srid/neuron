1844aae239e67c73ec0398b3d5682d7bd3396b97
Jared Weakly <jaredweakly@gmail.com>
2020-04-27 13:32:46 -0700

    Fix #56 in neuron-search (#138)
    * Fix potential omission of filename
    
    When the number of files in a directory is one, `rg --no-heading --.... *.md` will show the output
    
    ```sh
    ❯ rg --no-heading --no-line-number --sort path "title: " *.md
    title: Test2
    ```
    
    instead of the expected
    
    ```sh
    ❯ rg --with-filename --no-heading --no-line-number --sort path "title: " *.md
    2016603.md:title: Test2
    ```
    
    Now the behavior is consistent regardless of the number of files in a directory.
    
    * Remove ctrl-j binding

--- src-bash/neuron-search/default.nix
+++ src-bash/neuron-search/default.nix
@@ -11,10 +11,9 @@ pkgs.writeShellScriptBin "neuron-search"
     SEARCHFROMFIELD=''${3}
     OPENCMD=`${pkgs.envsubst}/bin/envsubst -no-unset -no-empty <<< ''${4}`
     cd ''${NOTESDIR}
-    ${pkgs.ripgrep}/bin/rg --no-heading --no-line-number --sort path "''${FILTERBY}" *.md \
+    ${pkgs.ripgrep}/bin/rg --no-heading --no-line-number --with-filename --sort path "''${FILTERBY}" *.md \
       | ${pkgs.fzf}/bin/fzf --tac --no-sort -d ':' -n ''${SEARCHFROMFIELD}.. \
         --preview '${pkgs.bat}/bin/bat --style=plain --color=always {1}' \
-        --bind 'ctrl-j:execute(xdg-open https://localhost:8080/(echo {1} | ${pkgs.gnused}/bin/sed "s/\.md/.html/g"))+abort' \
       | ${pkgs.gawk}/bin/awk -F: "{printf \"''${NOTESDIR}/%s\", \$1}" \
       | ${pkgs.findutils}/bin/xargs -r ''${OPENCMD}
   ''
