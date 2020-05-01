cb33bc23c2cdd024e3ab0c4d1e2e4b69329d72e6
Sridhar Ratnakumar <srid@srid.ca>
2020-04-30 21:56:29 -0400

    Fix tests
    Sorting within the same day is not supported anymore.

--- test/Neuron/Zettelkasten/ZettelSpec.hs
+++ test/Neuron/Zettelkasten/ZettelSpec.hs
@@ -22,15 +22,11 @@ spec = do
     let mkDay = fromGregorian 2020 3
         dummyContent = either error id $ parsePure "<spec>" "Dummy"
         mkZettel day idx =
-          Zettel (ZettelDateID (mkDay day) idx) "Some title" [Tag "science", Tag "journal/class"] Nothing dummyContent
+          Zettel (ZettelDateID (mkDay day) idx) "Some title" [Tag "science", Tag "journal/class"] (Just $ mkDay day) dummyContent
     it "sorts correctly" $ do
       let zs = [mkZettel 3 2, mkZettel 5 1]
       sortZettelsReverseChronological zs
         `shouldBe` [mkZettel 5 1, mkZettel 3 2]
-    it "sorts correctly even if same day" $ do
-      let zs = [mkZettel 7 2, mkZettel 7 4]
-      sortZettelsReverseChronological zs
-        `shouldBe` [mkZettel 7 4, mkZettel 7 2]
   describe "Zettel JSON" $ do
     let day = fromGregorian 2020 3 19
         zid = ZettelCustomID "Foo-Bar"
