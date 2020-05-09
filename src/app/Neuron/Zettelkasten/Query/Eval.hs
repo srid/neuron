a9c6e9997c706435c8277df7f9930228ad5190c2
Sridhar Ratnakumar <srid@srid.ca>
2020-05-09 18:55:51 -0400

    Render tag tree in AST

--- src/app/Neuron/Zettelkasten/Query/Eval.hs
+++ src/app/Neuron/Zettelkasten/Query/Eval.hs
@@ -8,24 +8,20 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Neuron.Zettelkasten.Query.Eval
-  ( evalZettelLinks,
-    expandQueries,
+  ( expandQueries,
   )
 where
 
 import Control.Monad.Except
 import Control.Monad.Writer
 import Data.Dependent.Sum
-import qualified Data.Map.Strict as Map
 import Data.Some
-import Data.Traversable (for)
-import Lucid
 import Neuron.Markdown
 import Neuron.Zettelkasten.Connection
 import Neuron.Zettelkasten.Query
 import Neuron.Zettelkasten.Query.Error
 import Neuron.Zettelkasten.Query.Parser (queryFromMarkdownLink)
-import Neuron.Zettelkasten.Query.View (buildQueryView, renderQueryLink)
+import Neuron.Zettelkasten.Query.View (buildQueryView)
 import Neuron.Zettelkasten.Zettel
 import Relude
 import qualified Text.Pandoc.Builder as B
@@ -83,49 +79,6 @@ expandQueries z@Zettel {..} = do
                 Just . (uri,) <$> withExcept Right (buildQueryView qres)
     -- FIXME: This is ugly; need to handle at type level.
     -- Right _ -> throwError $ Left $ QueryParseError_BadLocation uri
-    getConnections :: DSum Query Identity -> [(Connection, Zettel)]
-    getConnections = \case
-      Query_ZettelByID _ mconn :=> Identity mres ->
-        maybe [] pure $ (fromMaybe Folgezettel mconn,) <$> mres
-      Query_ZettelsByTag _ mconn _mview :=> Identity res ->
-        (fromMaybe Folgezettel mconn,) <$> res
-      _ ->
-        []
-
-type EvalResult = ([(Connection, Zettel)], Html ())
-
--- | Evaluate all queries in a zettel
---
--- Return the queries with results as a map from the original markdown link.
-evalZettelLinks ::
-  MonadError QueryError m =>
-  [Zettel] ->
-  Zettel ->
-  m (Map MarkdownLink EvalResult)
-evalZettelLinks zettels Zettel {..} =
-  fmap (Map.fromList . catMaybes) $ for (extractAutoLinks zettelContent) $ \ml ->
-    fmap (ml,) <$> evalMarkdownLink zettels ml
-
--- | Fully evaluate the query in a markdown link.
-evalMarkdownLink ::
-  MonadError QueryError m =>
-  [Zettel] ->
-  MarkdownLink ->
-  m (Maybe EvalResult)
-evalMarkdownLink zettels ml =
-  liftEither $ runExcept $ do
-    mq <- withExcept Left $ queryFromMarkdownLink ml
-    case mq of
-      Nothing -> pure Nothing
-      Just someQ -> Just <$> do
-        let qres = withSome someQ $ \q ->
-              q :=> Identity (runQuery zettels q)
-            conns = getConnections qres
-        view <-
-          withExcept Right $
-            renderQueryLink qres
-        pure (conns, view)
-  where
     getConnections :: DSum Query Identity -> [(Connection, Zettel)]
     getConnections = \case
       Query_ZettelByID _ mconn :=> Identity mres ->
--- src/app/Neuron/Zettelkasten/Query/View.hs
+++ src/app/Neuron/Zettelkasten/Query/View.hs
@@ -11,8 +11,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Neuron.Zettelkasten.Query.View
-  ( renderQueryLink,
-    renderZettelLink,
+  ( renderZettelLink,
     buildQueryView,
   )
 where
@@ -53,8 +52,10 @@ buildQueryView = \case
         buildZettelsLinks res
       True ->
         B.Para [B.Str "Zettel Links, grouped TODO"]
-  _ ->
-    pure $ Left $ B.Link mempty [B.Str $ "TODO:"] ("/unkonwn", "No title")
+  _q@(Query_Tags _) :=> Identity res -> pure $ Right $ do
+    -- Render a list of tags
+    -- toHtml $ Some q
+    renderTagTree $ foldTagTree $ tagTree res
   where
     mkAttr cls kvs =
       ("", words cls, kvs)
@@ -70,8 +71,8 @@ buildQueryView = \case
       B.Para [B.Str "Zettel Links TODO"]
 
 -- | Render the custom view for the given evaluated query
-renderQueryLink :: forall m. (MonadError QueryResultError m) => DSum Query Identity -> m (Html ())
-renderQueryLink = \case
+_renderQueryLink :: forall m. (MonadError QueryResultError m) => DSum Query Identity -> m (Html ())
+_renderQueryLink = \case
   Query_ZettelByID zid _mconn :=> Identity mres ->
     case mres of
       Nothing -> throwError $ QueryResultError_NoSuchZettel zid
@@ -89,10 +90,10 @@ renderQueryLink = \case
             i_ [class_ "tag icon"] mempty
             toHtml $ unTag tag
           renderZettelLinks (zettelsViewLinkView view) zettelGrp
-  q@(Query_Tags _) :=> Identity res -> pure $ do
+  q@(Query_Tags _) :=> Identity _res -> pure $ do
     -- Render a list of tags
+    -- renderTagTree $ foldTagTree $ tagTree res
     toHtml $ Some q
-    renderTagTree $ foldTagTree $ tagTree res
   where
     -- TODO: Instead of doing this here, group the results in runQuery itself.
     groupZettelsByTagsMatching pats matches =
@@ -105,6 +106,7 @@ renderQueryLink = \case
           li_ $ renderZettelLink (Just ltheme) z
 
 -- | Render a link to an individual zettel.
+-- TODO: Remove and consolidate with pandoc renderer
 renderZettelLink :: forall m. Monad m => Maybe LinkView -> Zettel -> HtmlT m ()
 renderZettelLink (fromMaybe def -> LinkView {..}) Zettel {..} = do
   let zurl = Rib.routeUrlRel $ Route_Zettel zettelID
@@ -136,40 +138,39 @@ renderZettelLink (fromMaybe def -> LinkView {..}) Zettel {..} = do
         ]
 
 -- |  Render a tag tree along with the count of zettels tagged with it
-renderTagTree :: forall m. Monad m => Forest (NonEmpty TagNode, Natural) -> HtmlT m ()
+renderTagTree :: Forest (NonEmpty TagNode, Natural) -> B.Block
 renderTagTree t =
-  div_ [class_ "tag-tree"] $
+  B.Div (mkAttr "tag-tree" mempty) $
     renderForest mempty t
   where
-    renderForest :: [TagNode] -> Forest (NonEmpty TagNode, Natural) -> HtmlT m ()
+    renderForest :: [TagNode] -> Forest (NonEmpty TagNode, Natural) -> [B.Block]
     renderForest ancestors forest =
-      ul_ $ do
-        forM_ forest $ \tree -> do
-          li_ $ renderTree ancestors tree
-    renderTree :: [TagNode] -> Tree (NonEmpty TagNode, Natural) -> HtmlT m ()
-    renderTree ancestors (Node (tagNode, count) children) = do
-      renderTag ancestors (tagNode, count)
-      whenNotNull children $
-        renderForest (ancestors <> toList tagNode) . toList
-    renderTag :: [TagNode] -> (NonEmpty TagNode, Natural) -> HtmlT m ()
-    renderTag ancestors (tagNode, count) = do
-      div_ [class_ "node"] $ do
-        let Tag tag = constructTag $ maybe tagNode (<> tagNode) $ nonEmpty ancestors
-            attrs = case count of
-              0 ->
-                [class_ "inactive"]
-              _ ->
-                [ href_ $ routeUrlRelWithQuery Route_Search [queryKey|tag|] tag,
-                  title_ $ show count <> " zettels tagged"
-                ]
-        a_ attrs $ renderTagNode tagNode
-    renderTagNode :: NonEmpty TagNode -> HtmlT m ()
+      pure $ B.BulletList $
+        forest <&> \tree ->
+          renderTree ancestors tree
+    renderTree :: [TagNode] -> Tree (NonEmpty TagNode, Natural) -> [B.Block]
+    renderTree ancestors (Node (tagNode, count) children) =
+      mconcat
+        [ [renderTag ancestors (tagNode, count)],
+          renderForest (ancestors <> toList tagNode) $ toList children
+        ]
+    renderTag :: [TagNode] -> (NonEmpty TagNode, Natural) -> B.Block
+    renderTag ancestors (tagNode, count) =
+      let Tag tag = constructTag $ maybe tagNode (<> tagNode) $ nonEmpty ancestors
+          url = routeUrlRelWithQuery Route_Search [queryKey|tag|] tag
+          tit = show count <> " zettels tagged"
+       in B.Div (mkAttr "node" mempty) $ pure $ B.Plain $ pure $
+            B.Link
+              (mkAttr (bool "" "inactive" $ count == 0) mempty)
+              [B.Str $ renderTagNode tagNode]
+              (url, tit)
+    renderTagNode :: NonEmpty TagNode -> Text
     renderTagNode = \case
       n :| (nonEmpty -> mrest) ->
         case mrest of
           Nothing ->
-            toHtml $ unTagNode n
-          Just rest -> do
-            toHtml $ unTagNode n
-            "/"
-            renderTagNode rest
+            unTagNode n
+          Just rest ->
+            unTagNode n <> "/" <> renderTagNode rest
+    mkAttr cls kvs =
+      ("", words cls, kvs)
