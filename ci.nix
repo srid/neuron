3a08665c668a8b70504246eb134322f3edbc2fe7
Sridhar Ratnakumar <srid@srid.ca>
2020-05-14 18:20:50 -0400

    Trigger CI

--- ci.nix
+++ ci.nix
@@ -5,6 +5,7 @@ builtins.mapAttrs (system: _v:
     pkgs = import (builtins.fetchTarball url) { inherit system; };
   in
   pkgs.recurseIntoAttrs {
+  
     # Build both default.nix and shell.nix so that both derivations are pushed
     # to cachix. This allows the development workflow (bin/run, etc.) to use
     # cachix to full extent.
