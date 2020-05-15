9ce94b8c7c0daaa9c9e2ce95d81c6dc02be9c7f0
Sridhar Ratnakumar <srid@srid.ca>
2020-05-15 17:43:19 -0400

    Trigger CI, with ver 0.7 of hercules-agent

--- ci.nix
+++ ci.nix
@@ -1,7 +1,7 @@
 builtins.mapAttrs (system: _v:
   let
     project = import ./project.nix { inherit system; };
-    url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/2255f292.tar.gz";
     pkgs = import (builtins.fetchTarball url) { inherit system; };
   in
   pkgs.recurseIntoAttrs {
