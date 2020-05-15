495fa071e46fe493cc5818262ad07236e2259eec
Sridhar Ratnakumar <srid@srid.ca>
2020-05-15 18:50:32 -0400

    Trigger CI

--- ci.nix
+++ ci.nix
@@ -5,9 +5,9 @@ builtins.mapAttrs (system: _v:
     pkgs = import (builtins.fetchTarball url) { inherit system; };
   in
   pkgs.recurseIntoAttrs {
-    # Build both default.nix and shell.nix so that both derivations are pushed
-    # to cachix. This allows the development workflow (bin/run, etc.) to use
-    # cachix to full extent.
+    # Build both default.nix and shell.nix such that both derivations are
+    # pushed to cachix. This allows the development workflow (bin/run, etc.) to
+    # use cachix to full extent.
     neuron = project.ghc.neuron;
     neuronShell = project.shells.ghc;
   }
