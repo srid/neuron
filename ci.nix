9207f15dafbee07a2c5e424557bf519eb0c51637
Sridhar Ratnakumar <srid@srid.ca>
2020-05-23 20:24:38 -0400

    Push nix-shell cache as well

--- ci.nix
+++ ci.nix
@@ -1,17 +1,11 @@
-builtins.mapAttrs (system: _v:
-  let
-    project = import ./project.nix { inherit system; };
-    url = "https://github.com/NixOS/nixpkgs/archive/2255f292.tar.gz";
-    pkgs = import (builtins.fetchTarball url) { inherit system; };
-  in
-  pkgs.recurseIntoAttrs {
-    # Build both default.nix and shell.nix such that both derivations are
-    # pushed to cachix. This allows the development workflow (bin/run, etc.) to
-    # use cachix to full extent.
-    neuron = project.ghc.neuron;
-    neuronShell = project.shells.ghc;
-  }
-) {
-  x86_64-linux = {};
-  x86_64-darwin = {};
+let
+  project = import ./project.nix { };
+  pkgs = import <nixpkgs> { };
+in
+pkgs.recurseIntoAttrs {
+  # Build both default.nix and shell.nix such that both derivations are
+  # pushed to cachix. This allows the development workflow (bin/run, etc.) to
+  # use cachix to full extent.
+  neuron = project.ghc.neuron;
+  neuronShell = project.shells.ghc;
 }
