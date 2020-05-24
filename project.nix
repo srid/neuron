59f55011105dd9f3530eab9de10a8fffc9eb59e8
Sridhar Ratnakumar <srid@srid.ca>
2020-05-24 11:11:51 -0400

    Use justStaticExecutables when building

--- project.nix
+++ project.nix
@@ -88,9 +88,9 @@ in {
       enableSeparateDataOutput = true;
     });
   in {
-    neuron = dontHaddock(super.neuron.overrideDerivation (drv: {
+    neuron = justStaticExecutables (dontHaddock (super.neuron.overrideDerivation (drv: {
         propagatedBuildInputs = drv.propagatedBuildInputs ++ [neuronSearchScript];
-    }));
+    })));
 
     shake = dontCheck super.shake;
     modern-uri = dontCheck (self.callHackageDirect {
