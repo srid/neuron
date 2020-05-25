d869199c9727e047a7b9257b2440b18a145c3dcb
Sridhar Ratnakumar <srid@srid.ca>
2020-05-25 17:35:28 -0400

    Factor out mkZettelGraph

--- neuron/src/lib/Neuron/Zettelkasten/Graph/Build.hs
+++ neuron/src/lib/Neuron/Zettelkasten/Graph/Build.hs
@@ -8,7 +8,6 @@ module Neuron.Zettelkasten.Graph.Build where
 import Control.Monad.Writer (runWriterT)
 import qualified Data.Graph.Labelled as G
 import qualified Data.Map.Strict as Map
-import Data.Traversable (for)
 import Neuron.Zettelkasten.Connection
 import Neuron.Zettelkasten.Graph.Type
 import Neuron.Zettelkasten.ID
@@ -16,6 +15,7 @@ import Neuron.Zettelkasten.Query.Error (QueryParseError)
 import Neuron.Zettelkasten.Query.Eval (queryConnections)
 import Neuron.Zettelkasten.Zettel
 import Relude
+import Text.Pandoc.Definition (Pandoc)
 
 -- | Build the Zettelkasten graph from a list of zettels
 --
@@ -26,18 +26,28 @@ mkZettelGraph ::
   ( ZettelGraph,
     Map ZettelID [QueryParseError]
   )
-mkZettelGraph zettels =
-  let res :: [(Zettel, ([(Maybe Connection, Zettel)], [QueryParseError]))] =
-        flip runReader (fmap (fst . unPandocZettel) zettels) $ do
-          for zettels $ \(PandocZettel (z, body)) -> fmap (z,) $ do
-            runWriterT $ queryConnections body
-      g :: ZettelGraph = G.mkGraphFrom (fst <$> res) $ flip concatMap res $ \(z1, fst -> conns) ->
-        conns <&> \(c, z2) -> (connectionMonoid (fromMaybe Folgezettel c), z1, z2)
-   in ( g,
-        Map.fromList $ flip mapMaybe res $ \(z, (_conns, errs)) ->
-          if null errs
-            then Nothing
-            else Just (zettelID z, errs)
-      )
+mkZettelGraph pZettels =
+  let zettels = fmap (fst . unPandocZettel) pZettels
+      res :: [(Zettel, ([(Maybe Connection, Zettel)], [QueryParseError]))] =
+        flip fmap pZettels $ \(PandocZettel (z, doc)) ->
+          (z, runQueryConnections zettels doc)
+      g :: ZettelGraph = G.mkGraphFrom zettels $ flip concatMap res $ \(z1, fst -> conns) ->
+        edgeFromConnection z1 <$> conns
+      errors = Map.fromList $ flip mapMaybe res $ \(z, (snd -> errs)) ->
+        if null errs
+          then Nothing
+          else Just (zettelID z, errs)
+   in (g, errors)
+
+runQueryConnections :: [Zettel] -> Pandoc -> ([(Maybe Connection, Zettel)], [QueryParseError])
+runQueryConnections zettels doc =
+  flip runReader zettels $ do
+    runWriterT $ queryConnections doc
+
+edgeFromConnection :: Zettel -> (Maybe Connection, Zettel) -> (Maybe Connection, Zettel, Zettel)
+edgeFromConnection z (c, z2) =
+  (connectionMonoid $ fromMaybe Folgezettel c, z, z2)
   where
+    -- Our connection monoid will never be Nothing (mempty); see the note in
+    -- type `ZettelGraph`.
     connectionMonoid = Just
