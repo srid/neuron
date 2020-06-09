fbe770900037d93a6bbdfd47e18e96ea066aefdc
Sridhar Ratnakumar <srid@srid.ca>
2020-06-09 13:26:09 -0400

    Fix tests

--- neuron/test/Neuron/Zettelkasten/ZettelSpec.hs
+++ neuron/test/Neuron/Zettelkasten/ZettelSpec.hs
@@ -19,12 +19,21 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
+  let noQueries = mempty -- TODO: test queries
+      noError = Right mempty
+      noContent = MetadataOnly ()
   describe "sortZettelsReverseChronological" $ do
     let mkDay = fromGregorian 2020 3
         (_ :: Meta, _dummyContent) = either (error . show) id $ parseMarkdown "<spec>" "Dummy"
-        noQueries = (mempty, mempty) -- TODO: test queries
         mkZettel day idx =
-          Zettel (ZettelDateID (mkDay day) idx) "Some title" [Tag "science", Tag "journal/class"] (Just $ mkDay day) noQueries
+          Zettel
+            (ZettelDateID (mkDay day) idx)
+            "Some title"
+            [Tag "science", Tag "journal/class"]
+            (Just $ mkDay day)
+            noQueries
+            noError
+            noContent
     it "sorts correctly" $ do
       let zs = [mkZettel 3 2, mkZettel 5 1]
       sortZettelsReverseChronological zs
@@ -32,9 +41,16 @@ spec = do
   describe "Zettel JSON" $ do
     let day = fromGregorian 2020 3 19
         zid = ZettelCustomID "Foo-Bar"
-        noQueries = (mempty, mempty) -- TODO: test queries
         (_ :: Meta, _dummyContent) = either (error . show) id $ parseMarkdown "<spec>" "Dummy"
-        zettel = Zettel zid "Some title" [Tag "science", Tag "journal/class"] (Just day) noQueries
+        zettel =
+          Zettel
+            zid
+            "Some title"
+            [Tag "science", Tag "journal/class"]
+            (Just day)
+            noQueries
+            noError
+            noContent
     it "Produces expected json" $ do
       -- "{\"id\":\"2011401\",\"title\":\"Some title\",\"tags\":[\"science\"]}"
       object (zettelJson zettel)
