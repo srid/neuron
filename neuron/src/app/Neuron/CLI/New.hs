38c8d86e2df565f12458430556a63613cd4f1a15
thebarbershaveshimself <ace8@me.com>
2020-06-12 12:10:27 -0700

    now using cmd to open file

--- neuron/src/app/Neuron/CLI/New.hs
+++ neuron/src/app/Neuron/CLI/New.hs
@@ -16,18 +16,17 @@ import Data.Some
 import Data.Text (strip)
 import qualified Data.Text as T
 import qualified Data.YAML as YAML
-import Development.Shake (Action)
-import Neuron.CLI.Types
+import Development.Shake (Action, CmdOption(..), cmd)
+import Neuron.CLI.Types hiding (cmd)
 import Neuron.Web.Generate as Gen
 import Neuron.Zettelkasten.ID (ZettelID, zettelIDSourceFileName)
 import qualified Neuron.Zettelkasten.ID.Scheme as IDScheme
 import Neuron.Zettelkasten.Zettel (zettelID)
-import Options.Applicative
+import Options.Applicative hiding (command)
 import Relude
 import qualified Rib
 import System.FilePath
 import qualified System.Posix.Env as Env
-import System.Posix.Process
 
 -- | Create a new zettel file and open it in editor if requested
 --
@@ -45,11 +44,11 @@ newZettelFile NewCommand {..} = do
   case mzid of
     Left e -> die $ show e
     Right zid -> do
-      path <- zettelPath zid
+      (notesDir, filename) <- zettelPath zid
       liftIO $ do
-        fileAction :: FilePath -> IO () <-
+        fileAction :: (FilePath, FilePath) -> IO () <-
           bool (pure showAction) mkEditActionFromEnv edit
-        writeFileText path $
+        writeFileText (notesDir </> filename) $
           T.intercalate
             "\n"
             [ "---",
@@ -58,31 +57,28 @@ newZettelFile NewCommand {..} = do
               "---",
               "\n"
             ]
-        fileAction path
+        fileAction (notesDir, filename)
   where
-    mkEditActionFromEnv :: IO (FilePath -> IO ())
+    mkEditActionFromEnv :: IO ((FilePath, FilePath) -> IO ())
     mkEditActionFromEnv =
       getEnvNonEmpty "EDITOR" >>= \case
         Nothing ->
           die "\n-e option can only be used with EDITOR environment variable set"
         Just editor ->
           pure $ editAction editor
-    editAction editor path = do
+    editAction editor (notesDir, filename) = do
       -- Show it first in case the editor launch fails
-      showAction path
-      let escapeSpaces (' ':xs) = '\\' : ' ' : escapeSpaces xs
-          escapeSpaces (x:xs) = x : escapeSpaces xs
-          escapeSpaces [] = []
-      executeFile "bash" True ["-c", editor ++ ' ' : escapeSpaces path ] Nothing
+      showAction (notesDir, filename)
+      cmd (Cwd notesDir) Shell editor [filename] :: IO ()
     showAction =
-      putStrLn
+      putStrLn . uncurry (</>)
     getEnvNonEmpty name =
       Env.getEnv name >>= \case
         Nothing -> pure Nothing
         Just (toString . strip . toText -> v) ->
           if null v then pure Nothing else pure (Just v)
 
-zettelPath :: ZettelID -> Action FilePath
+zettelPath :: ZettelID -> Action (FilePath, FilePath) 
 zettelPath zid = do
   notesDir <- Rib.ribInputDir
-  pure $ notesDir </> zettelIDSourceFileName zid
+  pure $ (notesDir , zettelIDSourceFileName zid)
