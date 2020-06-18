931288d06e6b9a9db14fad7536ca885b4547f0b6
Sridhar Ratnakumar <srid@srid.ca>
2020-06-18 18:34:21 -0400

    Discard more references

--- default.nix
+++ default.nix
@@ -3,12 +3,15 @@ let
   neuron = ghc.neuron;
 in 
   neuron.overrideDerivation (drv: {
-     disallowedReferences = [ ghc.pandoc ghc.pandoc-types ghc.shake ghc.warp ghc.HTTP ];
+     disallowedReferences = [ ghc.pandoc ghc.pandoc-types ghc.shake ghc.warp ghc.HTTP ghc.js-jquery ghc.js-dgtable ghc.js-flot ];
      postInstall = ''
       remove-references-to -t ${ghc.pandoc} $out/bin/neuron
       remove-references-to -t ${ghc.pandoc-types} $out/bin/neuron
       remove-references-to -t ${ghc.shake} $out/bin/neuron
       remove-references-to -t ${ghc.warp} $out/bin/neuron
       remove-references-to -t ${ghc.HTTP} $out/bin/neuron
+      remove-references-to -t ${ghc.js-jquery} $out/bin/neuron
+      remove-references-to -t ${ghc.js-dgtable} $out/bin/neuron
+      remove-references-to -t ${ghc.js-flot} $out/bin/neuron
      '';
   })
