7ee135c044a616b8cca565624412b75d4bb40211
Sridhar Ratnakumar <srid@srid.ca>
2020-06-18 17:59:09 -0400

    Remove Haskell refs from final neuron drv

--- default.nix
+++ default.nix
@@ -1,1 +1,14 @@
-(import ./project.nix {}).ghc.neuron
+let 
+  ghc = (import ./project.nix {}).ghc;
+  neuron = ghc.neuron;
+in 
+  neuron.overrideDerivation (drv: {
+     disallowedReferences = [ ghc.pandoc ghc.pandoc-types ghc.shake ghc.warp ghc.HTTP ];
+     postInstall = ''
+      remove-references-to -t ${ghc.pandoc} $out/bin/neuron
+      remove-references-to -t ${ghc.pandoc-types} $out/bin/neuron
+      remove-references-to -t ${ghc.shake} $out/bin/neuron
+      remove-references-to -t ${ghc.warp} $out/bin/neuron
+      remove-references-to -t ${ghc.HTTP} $out/bin/neuron
+     '';
+  })
