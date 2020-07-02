427e1e8ef8abbbd5bb79c1af267221c1f6e3df65
felko <baxbaxwalanuksiwe@gmail.com>
2020-07-02 12:41:11 +0200

    Implement [[z:/id]] links

--- neuron/src/app/Neuron/Org.hs
+++ neuron/src/app/Neuron/Org.hs
@@ -2,11 +2,14 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Neuron.Org where
 
 import Data.TagTree
+import qualified Data.Text as Text
+import qualified Data.Text.Read as Text
 import Neuron.Zettelkasten.Zettel.Meta
 import Neuron.Zettelkasten.Zettel.ParseError
 import Relude
@@ -15,6 +18,7 @@ import qualified Text.Pandoc.Builder as B
 import Text.Pandoc.Definition hiding (Meta (..))
 import Text.Pandoc.Readers.Org (readOrg)
 import Text.Pandoc.Util
+import qualified Text.Pandoc.Walk as W
 import Prelude (lookup)
 
 parseOrg ::
@@ -25,7 +29,7 @@ parseOrg _ s =
   case runPure (readOrg def s) of
     Left e -> Left $ ZettelParseError_InvalidOrg (show e)
     Right (Pandoc _ body) ->
-      Right (extractMetadata body, Pandoc mempty body)
+      Right (extractMetadata body, rewriteLinks $ Pandoc mempty body)
 
 extractMetadata :: [B.Block] -> Maybe Meta
 extractMetadata body = do
@@ -35,3 +39,21 @@ extractMetadata body = do
   let title = Nothing
       tags = fmap Tag . words <$> lookup "tags" properties
   pure $ Meta {..}
+
+rewriteLinks :: Pandoc -> Pandoc
+rewriteLinks = W.walk \case
+  -- REVIEW What should I do with the title?
+  link@(Link attr _ (src, title)) ->
+    let processQuery (query, rest)
+          | Text.null rest = Link attr [Str query] (query, title)
+          | otherwise = link
+     in either (const link) processQuery $ queryReader src
+  inline -> inline
+
+-- REVIEW Use Text.URI?
+queryReader :: Text.Reader Text
+queryReader src
+  | auth == "z:/" = Right ("z:zettel/" <> rest, "")
+  | otherwise = Left "No"
+  where
+    (auth, rest) = Text.splitAt 3 src
