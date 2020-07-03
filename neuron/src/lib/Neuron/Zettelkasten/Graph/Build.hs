49c0fad0d1c8ea8b083b9b861dd692f1896669c5
felko <baxbaxwalanuksiwe@gmail.com>
2020-07-03 15:01:56 +0200

    Format code

--- neuron/src/lib/Neuron/Zettelkasten/Graph/Build.hs
+++ neuron/src/lib/Neuron/Zettelkasten/Graph/Build.hs
@@ -17,8 +17,8 @@ import Neuron.Zettelkasten.ID
 import Neuron.Zettelkasten.Query.Error (QueryError)
 import Neuron.Zettelkasten.Query.Eval (queryConnections)
 import Neuron.Zettelkasten.Zettel
-import Neuron.Zettelkasten.Zettel.Parser
 import Neuron.Zettelkasten.Zettel.Format
+import Neuron.Zettelkasten.Zettel.Parser
 import Relude
 import Relude.Extra.Group
 import System.FilePath
@@ -30,20 +30,25 @@ buildZettelkasten ::
     Map ZettelID ZettelError
   )
 buildZettelkasten filesPerFormat =
-  let allFiles = concatMap (\ (_, _, filesWithContent) -> fmap fst filesWithContent) filesPerFormat
+  let allFiles = concatMap (\(_, _, filesWithContent) -> fmap fst filesWithContent) filesPerFormat
       groupedFiles = groupBy (toText . takeBaseName) allFiles
       duplicates = Map.filter (\files -> length files > 1) groupedFiles
       filterUnique (format, zreader, filesWithContent) =
-        (format, zreader, filter (\(file, _) ->
-                                    Map.notMember (toText $ takeBaseName file) duplicates) filesWithContent)
+        ( format,
+          zreader,
+          filter
+            (\(file, _) -> Map.notMember (toText $ takeBaseName file) duplicates)
+            filesWithContent
+        )
       zs = parseZettels $ fmap filterUnique filesPerFormat
       parseErrors = Map.fromList $ lefts zs <&> (zettelID &&& zettelError)
       (g, queryErrors) = mkZettelGraph $ fmap sansContent zs
-      errors = Map.unions
-        [ fmap ZettelError_ParseError parseErrors,
-          fmap ZettelError_QueryErrors queryErrors,
-          fmap ZettelError_DuplicateIDs $ Map.mapKeys parseZettelID duplicates
-        ]
+      errors =
+        Map.unions
+          [ fmap ZettelError_ParseError parseErrors,
+            fmap ZettelError_QueryErrors queryErrors,
+            fmap ZettelError_DuplicateIDs $ Map.mapKeys parseZettelID duplicates
+          ]
    in (g, zs, errors)
 
 -- | Build the Zettelkasten graph from a list of zettels
