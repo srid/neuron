dd3881a8599c8f5131db664175f49fe89362ba0f
Sridhar Ratnakumar <srid@srid.ca>
2020-07-19 17:39:35 -0400

    Base image is not required, apparently

--- docker.nix
+++ docker.nix
@@ -3,17 +3,11 @@
 #   docker load -i $(nix-build docker.nix)
 let
   pkgs = import <nixpkgs> {};
-  baseImage = pkgs.dockerTools.pullImage {
-    imageName = "lnl7/nix";
-    finalImageTag = "2.3.3";
-    imageDigest = "sha256:a969b4f238eab9298fe426fc90cc45efe01685431a419ca2907f10ddbbed2b7f";
-    sha256 = "1zy4zwlka5c4hgjbgy8xh1ikyb3z2lwzfjwv8y0cyzx1danys4vk";
-  };
+  neuron = import ./. {};
 in
 
 pkgs.dockerTools.buildImage {
-  fromImage = baseImage;
   name = "sridca/neuron";
   tag = "test";
-  contents = [ (import ./. {}) ];
+  contents = [ neuron ];
 }
