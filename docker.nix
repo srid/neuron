d668ea5773ca3a3a492ae47a5c341d4456480e4a
TheMatten <matten@tuta.io>
2020-07-20 13:51:02 +0200

    docker.nix: remove includeShell, add docs

--- docker.nix
+++ docker.nix
@@ -1,17 +1,18 @@
 # Builds a docker image containing the neuron executable
 # Run as:
-#   docker load -i $(nix-build docker.nix)
+#   docker load -i $(
+#     nix-build docker.nix \
+#       --arg name '"<image name>"' \
+#       --arg tag '"<image tag>"'
+#   )
 let
   pkgs = import <nixpkgs> {};
   neuron = import ./. {};
 in {
   name ? "sridca/neuron"
 , tag ? "test"
-, includeShell ? false
 }: pkgs.dockerTools.buildImage {
   name = name;
   tag = tag;
-  contents = [ neuron ] ++ (if includeShell
-    then [ pkgs.coreutils pkgs.bash_5 ]
-    else []);
+  contents = [ neuron pkgs.coreutils pkgs.bash_5 ];
 }
