a00ac033591990a4952902693923631c90cbdf9e
TheMatten <matten@tuta.io>
2020-07-20 13:55:39 +0200

    Make `docker.nix` configurable, add option to include shell (#299)
    * docker.nix: config arguments, includeShell option
    
    * docker.nix: remove includeShell, add docs
    
    * Add comment explaining why these are needed
    
    Co-authored-by: Sridhar Ratnakumar <srid@srid.ca>

--- docker.nix
+++ docker.nix
@@ -1,13 +1,23 @@
 # Builds a docker image containing the neuron executable
 # Run as:
-#   docker load -i $(nix-build docker.nix)
+#   docker load -i $(
+#     nix-build docker.nix \
+#       --arg name '"<image name>"' \
+#       --arg tag '"<image tag>"'
+#   )
 let
   pkgs = import <nixpkgs> {};
   neuron = import ./. {};
-in
-
-pkgs.dockerTools.buildImage {
-  name = "sridca/neuron";
-  tag = "test";
-  contents = [ neuron ];
+in {
+  name ? "sridca/neuron"
+, tag ? "test"
+}: pkgs.dockerTools.buildImage {
+  name = name;
+  tag = tag;
+  contents = [ 
+    neuron
+    # These are required for the GitLab CI runner
+    pkgs.coreutils 
+    pkgs.bash_5 
+  ];
 }
