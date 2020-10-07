e20b53a712d6fae9454e80f150cbb3178600891a
Sridhar Ratnakumar <srid@srid.ca>
2020-10-07 18:02:42 -0400

    This code is not needed to fix the bug

--- neuron/src/lib/Neuron/Zettelkasten/Query/Parser.hs
+++ neuron/src/lib/Neuron/Zettelkasten/Query/Parser.hs
@@ -22,7 +22,6 @@ where
 import Control.Monad.Except
 import Data.Some
 import Data.TagTree (TagNode (..), TagPattern, constructTag, mkTagPattern)
-import qualified Network.URI.Encode as E
 import Neuron.Reader.Type (ZettelFormat (..))
 import Neuron.Zettelkasten.Connection
 import Neuron.Zettelkasten.ID
@@ -65,7 +64,7 @@ queryFromURI defConn uri = do
               guard $ URI.uriAuthority uri == Left False
               (False, path) <- URI.uriPath uri
               pure path
-        (toText . E.decode . toString . URI.unRText -> path) :| [] <- hoistMaybe shortLinkPath
+        (URI.unRText -> path) :| [] <- hoistMaybe shortLinkPath
         zid <-
           hoistMaybe $
             -- Allow raw filename (ending with ".md"). HACK: hardcoding
