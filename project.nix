de594d34c9a62c21141bd634004c732b60e0d5cd
Sridhar Ratnakumar <srid@srid.ca>
2020-10-09 11:31:14 -0400

    Avoid having to pass pkgsForBins

--- project.nix
+++ project.nix
@@ -7,7 +7,7 @@ let
   nixpkgs = import sources.nixpkgs {};
 in {
   pkgs ? nixpkgs,
-  pkgsForBins ? nixpkgs,
+  pkgsForBins ? null,
   neuronFlags ? [],
   disableHsLuaTests ? false,
   withHoogle ? false,
@@ -36,7 +36,7 @@ let
     cp $src/src-bash/neuron-search $out/bin/neuron-search
     chmod +x $out/bin/neuron-search
     wrapProgram $out/bin/neuron-search --prefix 'PATH' ':' ${
-      with pkgsForBins;
+      with (if pkgsForBins != null then pkgsForBins else pkgs);
       lib.makeBinPath [ fzf ripgrep gawk bat findutils envsubst ]
     }
     PATH=$PATH:$out/bin
