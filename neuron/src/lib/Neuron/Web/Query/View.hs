2038b0cac0bb3332d3272908c1a0ab7a0cf2c1ab
Sridhar Ratnakumar <srid@srid.ca>
2020-12-04 14:29:06 -0500

    Make limit-indicator compact

--- neuron/src/lib/Neuron/Web/Query/View.hs
+++ neuron/src/lib/Neuron/Web/Query/View.hs
@@ -73,16 +73,16 @@ renderQueryResult minner = \case
               forM_ zettelGrp $ \z ->
                 el "li" $
                   renderZettelLink Nothing (Just conn) (Just $ zettelsViewLinkView view) z
-        else el "ul" $
+        else el "ul" $ do
           forM_ (maybe id (take . naturalToInt) (zettelsViewLimit view) $ res) $ \z -> do
             el "li" $
               renderZettelLink Nothing (Just conn) (Just $ zettelsViewLinkView view) z
-      forM_ (zettelsViewLimit view) $ \limit ->
-        elClass "div" "ui basic center aligned segment" $ do
-          el "em" $ do
-            let zettelCount = (intToNatural . length $ res)
-                limitHint = " (Displaying " <> (show $ min limit zettelCount) <> " out of " <> show zettelCount <> " zettels)"
-            text $ limitHint
+          forM_ (zettelsViewLimit view) $ \limit ->
+            el "li" $
+              elClass "span" "ui grey text" $ do
+                let zettelCount = (intToNatural . length $ res)
+                    limitHint = "(displaying only " <> (show $ min limit zettelCount) <> " out of " <> show zettelCount <> " zettels)"
+                text $ limitHint
   q@(ZettelQuery_Tags _) :=> Identity res -> do
     el "section" $ do
       renderQuery $ Some q
