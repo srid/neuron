9b4f3eef998d6d982aafeec5fc02ce5b1330acd7
Sridhar Ratnakumar <srid@srid.ca>
2020-12-09 16:58:00 -0500

    Drop commonnmark override, and update pandoc-link-context

--- project.nix
+++ project.nix
@@ -29,7 +29,6 @@ let
     # TODO: Switch to using `niv` exclusively.
     rib = thunkOrPath "rib";
     reflex-dom-pandoc = thunkOrPath "reflex-dom-pandoc";
-    commonmark = thunkOrPath "commonmark-hs";
     pandoc-link-context = thunkOrPath "pandoc-link-context";
   };
 
@@ -51,20 +50,9 @@ let
   haskellOverrides = self: super: {
     rib-core = self.callCabal2nix "rib-core" (sources.rib + "/rib-core") { };
 
-    # Use commonmark from git for bug fixes (not yet released)
-    commonmark = self.callCabal2nix "commonmark" (sources.commonmark + "/commonmark") {};
-    commonmark-extensions = self.callCabal2nix "commonmark-extensions" (sources.commonmark + "/commonmark-extensions") {};
-    commonmark-pandoc = self.callCabal2nix "commonmark-pandoc" (sources.commonmark + "/commonmark-pandoc") {};
-
     pandoc-link-context = self.callCabal2nix "pandoc-link-context" sources.pandoc-link-context {};
     reflex-dom-pandoc =
       pkgs.haskell.lib.dontHaddock (self.callCabal2nix "reflex-dom-pandoc" sources.reflex-dom-pandoc { });
-
-    # This version is not the default in the static nixpkgs, yet.
-    #skylighting = super.skylighting_0_10_0_2 or super.skylighting;
-    #skylighting-core = super.skylighting-core_0_10_0_2 or super.skylighting-core;
-    # Jailbreak pandoc to work with newer skylighting (and commonmark)
-    #pandoc = doJailbreak (dontCheck super.pandoc);
 
     # Test fails on pkgsMusl
     # https://github.com/hslua/hslua/issues/67
