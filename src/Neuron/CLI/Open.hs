33cc9b2c6425f69d585552e48f83f39b8e25562a
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-03-21 17:24:47 +0700

    fix error in CLI\Open

--- src/Neuron/CLI/Open.hs
+++ src/Neuron/CLI/Open.hs
@@ -13,15 +13,21 @@ import Relude
 import System.Directory (doesFileExist)
 import System.FilePath ((</>))
 import System.Info (os)
-import System.Posix.Process (executeFile)
+import System.Process
 
 openLocallyGeneratedFile :: (MonadIO m, MonadApp m, MonadFail m) => OpenCommand -> m ()
 openLocallyGeneratedFile (OpenCommand route) = do
   let relHtmlPath = routeHtmlPath `foldSome` route
-      opener = if os == "darwin" then "open" else "xdg-open"
+      opener = case os of
+                 "darwin" -> "open"
+                 "mingw32" -> "cmd"
+                 _ -> "xdg-open"
+      extraArg = if os == "mingw32" then
+                    ["/c", "start"]
+                  else []
   htmlPath <- fmap (</> relHtmlPath) getOutputDir
   liftIO (doesFileExist htmlPath) >>= \case
     False -> do
       fail "No generated HTML found. Try runing `neuron gen` first."
     True -> do
-      liftIO $ executeFile opener True [htmlPath] Nothing
+      liftIO $ callProcess opener $ extraArg ++ [htmlPath]
