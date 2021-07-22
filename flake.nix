dfdddee66232b054c554152effb95e5ed388574c
Sridhar Ratnakumar <srid@srid.ca>
2021-07-22 13:25:11 -0400

    Advance nixpkgs
    This may (positively) impact #626

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Future-proof note-taking and publishing based on Zettelkasten";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/123db833485edf8df83f95ef2888113390768686";
+    nixpkgs.url = "github:nixos/nixpkgs/16105403bdd843540cbef9c63fc0f16c1c6eaa70";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
