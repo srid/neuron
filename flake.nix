d79c1ea0d268c0773b7971f504467a8a9c7b503c
Sridhar Ratnakumar <srid@srid.ca>
2021-09-03 19:46:37 +0200

    Advance nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Future-proof note-taking and publishing based on Zettelkasten";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/002e0737ff526e15e30ce6be41fc0037d8e88ab7";
+    nixpkgs.url = "github:nixos/nixpkgs/08ef0f28e3a41424b92ba1d203de64257a9fca6a";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
