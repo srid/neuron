75505d4185cc70d908b6dd4b27b61cb669d17479
Sridhar Ratnakumar <srid@srid.ca>
2026-03-22 11:51:05 -0400

    Migrate to haskell-flake
    Replace manual Haskell package management with haskell-flake
    flake-parts module. Uses projectRoot for source filtering,
    otherOverlays for dep patches, and settings for jailbreak/haddock.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
 
     nix-filter.url = "github:numtide/nix-filter/3c9e33ed627e009428197b07216613206f06ed80";
     nix-filter.flake = false;
@@ -31,34 +32,30 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, nix-filter, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, haskell-flake, nix-filter, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ haskell-flake.flakeModule ];
+
       flake = {
         homeManagerModule = import ./home-manager-module.nix;
       };
 
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-      perSystem = { pkgs, system, ... }:
+      perSystem = { self', pkgs, lib, ... }:
         let
           nix-filter-lib = import nix-filter;
 
-          sources = {
-            neuron = nix-filter-lib {
-              root = ./.;
-              name = "neuron";
-              include = [
-                "neuron-search"
-                "neuron.cabal"
-                (nix-filter-lib.inDirectory "exe")
-                (nix-filter-lib.inDirectory "src")
-                (nix-filter-lib.inDirectory "test")
-              ];
-            };
-            reflex-dom-pandoc = inputs.reflex-dom-pandoc;
-            pandoc-link-context = inputs.pandoc-link-context;
-            directory-contents = inputs.directory-contents;
-            reflex-fsnotify = inputs.reflex-fsnotify;
+          neuronSource = nix-filter-lib {
+            root = ./.;
+            name = "neuron";
+            include = [
+              "neuron-search"
+              "neuron.cabal"
+              (nix-filter-lib.inDirectory "exe")
+              (nix-filter-lib.inDirectory "src")
+              (nix-filter-lib.inDirectory "test")
+            ];
           };
 
           searchBuilder = ''
@@ -71,94 +68,90 @@
             PATH=$PATH:$out/bin
           '';
 
-          wrapSearchScript = drv: {
-            buildTools = [ pkgs.makeWrapper ];
-            preConfigure = searchBuilder;
-          };
-
           inherit (pkgs.haskell.lib)
-            overrideCabal doJailbreak dontCheck dontHaddock justStaticExecutables appendConfigureFlags;
-
-          haskellOverrides = self: super: {
-            pandoc-link-context = doJailbreak (self.callCabal2nix "pandoc-link-context" sources.pandoc-link-context { });
-            reflex-dom-pandoc =
-              dontHaddock (doJailbreak (overrideCabal (self.callCabal2nix "reflex-dom-pandoc" sources.reflex-dom-pandoc { }) (old: {
-                postPatch = (old.postPatch or "") + ''
-                  # GHC 9.10: forM no longer in scope, add explicit import
-                  sed -i '/^import Control.Monad.Reader/a import Data.Traversable (forM)' src/Reflex/Dom/Pandoc/Footnotes.hs
-                  # GHC 9.10: ~ requires TypeOperators
-                  sed -i '1s/^/{-# LANGUAGE TypeOperators #-}\n/' src/Reflex/Dom/Pandoc/Raw.hs
-                  # pandoc-types >= 1.23: Null constructor removed, replace with blank pattern
-                  sed -i '/^  Null ->$/,/^    blank >> pure mempty$/d' src/Reflex/Dom/Pandoc/Document.hs
-                '';
-              })));
-            fsnotify = dontCheck super.fsnotify;
-            reflex-fsnotify =
-              doJailbreak (self.callCabal2nix "reflex-fsnotify" sources.reflex-fsnotify { });
-
-
-            # witherable 0.4.x (has Data.Witherable module that directory-contents needs)
-            witherable = doJailbreak (self.callHackageDirect {
-              pkg = "witherable";
-              ver = "0.4.2";
-              sha256 = "sha256-M5KOI2qKqf4qdfKUwQ2h+3pF5PfPhtz0dozPUAZVRL0=";
-            } { });
-            directory-contents = doJailbreak (self.callCabal2nix "directory-contents" sources.directory-contents { });
-
-            neuron = appendConfigureFlags
-              ((justStaticExecutables
-                (doJailbreak (overrideCabal (self.callCabal2nix "neuron" sources.neuron { })
-                  wrapSearchScript))).overrideDerivation (drv: {
-                disallowedReferences = [
-                  self.pandoc-types
-                  self.warp
-                  self.HTTP
-                  self.js-jquery
-                  self.js-dgtable
-                  self.js-flot
-                ];
-                postInstall = ''
-                  remove-references-to -t ${self.pandoc-types} $out/bin/neuron
-                  remove-references-to -t ${self.warp} $out/bin/neuron
-                  remove-references-to -t ${self.HTTP} $out/bin/neuron
-                  remove-references-to -t ${self.js-jquery} $out/bin/neuron
-                  remove-references-to -t ${self.js-dgtable} $out/bin/neuron
-                  remove-references-to -t ${self.js-flot} $out/bin/neuron
-                '';
-              }))
-              [ ];
-          };
-
-          haskellPackages = pkgs.haskellPackages.override {
-            overrides = haskellOverrides;
-          };
-
-          nixShellSearchScript = pkgs.stdenv.mkDerivation {
-            name = "neuron-search";
-            src = sources.neuron;
-            buildInputs = [ pkgs.makeWrapper ];
-            buildCommand = searchBuilder;
-          };
+            overrideCabal doJailbreak dontCheck dontHaddock justStaticExecutables;
         in
         {
-          formatter = pkgs.nixpkgs-fmt;
+          haskellProjects.default = {
+            projectRoot = neuronSource;
+            autoWire = [ "packages" "checks" "devShells" ];
 
-          packages = {
-            default = haskellPackages.neuron;
-            neuron = haskellPackages.neuron;
-          };
+            packages = {
+              reflex-dom-pandoc.source = inputs.reflex-dom-pandoc;
+              pandoc-link-context.source = inputs.pandoc-link-context;
+              directory-contents.source = inputs.directory-contents;
+              reflex-fsnotify.source = inputs.reflex-fsnotify;
+            };
 
-          devShells.default = haskellPackages.shellFor {
-            packages = p: [ p.neuron ];
-            buildInputs = [
-              pkgs.nixpkgs-fmt
-              haskellPackages.ghcid
-              haskellPackages.cabal-install
-              haskellPackages.hlint
-              haskellPackages.ormolu
-              nixShellSearchScript
+            settings = {
+              reflex-dom-pandoc.jailbreak = true;
+              reflex-dom-pandoc.haddock = false;
+              reflex-fsnotify.jailbreak = true;
+              directory-contents.jailbreak = true;
+              pandoc-link-context.jailbreak = true;
+              neuron.jailbreak = true;
+              neuron.buildFromSdist = false;
+            };
+
+            otherOverlays = [
+              (hself: hsuper: {
+                # Patch reflex-dom-pandoc for GHC 9.10 / pandoc-types 1.23
+                reflex-dom-pandoc = overrideCabal hsuper.reflex-dom-pandoc (old: {
+                  postPatch = (old.postPatch or "") + ''
+                    sed -i '/^import Control.Monad.Reader/a import Data.Traversable (forM)' src/Reflex/Dom/Pandoc/Footnotes.hs
+                    sed -i '1s/^/{-# LANGUAGE TypeOperators #-}\n/' src/Reflex/Dom/Pandoc/Raw.hs
+                    sed -i '/^  Null ->$/,/^    blank >> pure mempty$/d' src/Reflex/Dom/Pandoc/Document.hs
+                  '';
+                });
+                fsnotify = dontCheck hsuper.fsnotify;
+                # witherable 0.4.x (has Data.Witherable module)
+                witherable = doJailbreak (hself.callHackageDirect {
+                  pkg = "witherable";
+                  ver = "0.4.2";
+                  sha256 = "sha256-M5KOI2qKqf4qdfKUwQ2h+3pF5PfPhtz0dozPUAZVRL0=";
+                } { });
+                # neuron custom: wrap search script + strip references
+                neuron =
+                  let
+                    wrapped = overrideCabal hsuper.neuron (old: {
+                      buildTools = (old.buildTools or [ ]) ++ [ pkgs.makeWrapper ];
+                      preConfigure = searchBuilder;
+                    });
+                  in
+                  (justStaticExecutables wrapped).overrideDerivation (drv: {
+                    disallowedReferences = [
+                      hself.pandoc-types
+                      hself.warp
+                      hself.HTTP
+                      hself.js-jquery
+                      hself.js-dgtable
+                      hself.js-flot
+                    ];
+                    postInstall = ''
+                      remove-references-to -t ${hself.pandoc-types} $out/bin/neuron
+                      remove-references-to -t ${hself.warp} $out/bin/neuron
+                      remove-references-to -t ${hself.HTTP} $out/bin/neuron
+                      remove-references-to -t ${hself.js-jquery} $out/bin/neuron
+                      remove-references-to -t ${hself.js-dgtable} $out/bin/neuron
+                      remove-references-to -t ${hself.js-flot} $out/bin/neuron
+                    '';
+                  });
+              })
             ];
+
+            devShell = {
+              tools = hp: {
+                ghcid = hp.ghcid;
+                cabal-install = hp.cabal-install;
+                hlint = hp.hlint;
+                ormolu = hp.ormolu;
+              };
+              mkShellArgs.packages = [ pkgs.nixpkgs-fmt ];
+            };
           };
+
+          formatter = pkgs.nixpkgs-fmt;
+          packages.default = self'.packages.neuron;
         };
     };
 }
